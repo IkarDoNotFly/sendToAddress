d378d51d93ee02ef2e1561c0badc72431ca7dee8
vyacheslav.mikhalkin <slavikmikhalkin@gmail.com>
2021-06-29 19:20:02 +0300

    v1 38,41 strings doesn`t works

--- test.go
+++ test.go
@@ -1,35 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 )
+type Owner struct{
+	my_amount float64 `json:"my_amount"`
 
-func sendToAddress(address_inp string,amount_inp string){
-	//TODO:check amount from json_file,then check address of receiver(true\false) and update amount of users
+}
+type Accounts struct{
+	Accounts 	[]Account 	`json:"Accounts"`
+}
+
+type Account struct{
+	amount float64 `json:"amount"`
+	owner_address string `json:"owner_address"`
+}
+
+
+
+func sendToAddress(address_inp string,amount_inp float64){
+	jsonFile,err:=os.Open("accounts.json")
+
+	if err!=nil{
+		fmt.Println(err)
+	}else {
+		fmt.Println("Successfully Opened accounts.json")
+	}
+
+	var accounts Accounts
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(byteValue,&accounts)
+
+	for i:=0; i<len(accounts.Accounts);i++{
+		if Owner.my_amount<amount_inp && address_inp==accounts.Accounts.owner_address[i] {
+			//I don`t know how to work with it correctly(may be we will discuss it on interview)
+			Owner.my_amount-=amount_inp
+			accounts.Accounts.amount[i]+=amount_inp
+		}else{
+			fmt.Println("Sorry,we can`t do this.\n Your amount isn`t enough or address of recipient wrong!")
+		}
+	}
+
+
+	defer jsonFile.Close()
+
+
 	fmt.Println("Transaction done! Do you want create new operation?(Yes/No)")
+
+
 	var answer string
 	fmt.Fscan(os.Stdin,&answer)
 	if(answer=="No" || answer=="no"){
 		os.Exit(1)
 	}else{
-		fmt.Println("Input amount = ")
-		var amount,address string
-		fmt.Fscan(os.Stdin,&amount)
-
-		fmt.Println("Input address: ")
-		fmt.Fscan(os.Stdin,&address)
-		sendToAddress(amount,address)
+		inputData()
 	}
 
+}
+
+func inputData(){
+	var input=""
+	var address string
+	var amount float64
+	fmt.Println("Input amount = ")
+	fmt.Fscan(os.Stdin,&amount)
+
+	fmt.Println("Input address: ")
+	fmt.Fscan(os.Stdin,&input)
+	sendToAddress(address,amount)
 }
 
 func main() {
 	fmt.Println("Welcome,input your command(for more information input 'help'):")
 
 	var input=""
-	var amount,address string
+
 	for strings.TrimRight(input, "\n")!="exit"{
 		fmt.Fscan(os.Stdin, &input)
 		if(input=="help") {
@@ -37,12 +86,7 @@ func main() {
 		} else if(input=="exit"){
 			os.Exit(1)
 		} else if(input=="sendToAddress"){
-			fmt.Println("Input amount = ")
-			fmt.Fscan(os.Stdin,&amount)
-
-			fmt.Println("Input address: ")
-			fmt.Fscan(os.Stdin,&input)
-			sendToAddress(amount,address)
+			inputData()
 
 		} else{
 			fmt.Println("Invalid command! Input 'help' and try again.")
